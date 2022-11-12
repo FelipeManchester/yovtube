36c1db0baab10ea7ece3354d3d9a778cfb641074
Felipe Manchester <f.p2809@gmail.com>
2022-11-11 22:53:57 -0300

    dark mode

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import config from '../config.json';
 import styled from 'styled-components';
-import { CSSReset } from '../src/components/CSSReset';
 import Menu from '../src/components/Menu';
 import { StyledTimeline } from '../src/components/TimeLine';
 
@@ -15,7 +14,6 @@ function HomePage() {
 
   return (
     <>
-      <CSSReset />
       <div style={homePageStyles}>
         <Menu filterValue={filterValue} setFilterValue={setFilterValue} />
         <Header />
@@ -34,13 +32,13 @@ export default HomePage;
 // }
 
 const StyledHeader = styled.div`
+  background-color: ${({ theme }) => theme.backgroundLevel1};
   img {
     width: 80px;
     height: 80px;
     border-radius: 50%;
   }
   .user-info {
-    margin-top: 50px;
     display: flex;
     align-items: center;
     width: 100%;
